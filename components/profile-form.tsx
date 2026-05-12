ffe294e427291ed0190407a2236beb12a5942e95
Eimi Izquierdo <eimiceiz@gmail.com>
2026-05-11 22:32:48 -0600

    Corrección #2 api-client --> client-api

--- components/profile-form.tsx
+++ components/profile-form.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState, useEffect } from "react"
-import { updateUserProfile } from "@/lib/api-client"
+import { updateUserProfile } from "@/lib/client-api"
 import { useAuth } from "@/lib/auth-context"
 import type { UserLocal } from "@/lib/types/frontend-types"
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar"
